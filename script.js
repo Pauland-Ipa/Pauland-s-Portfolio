3c66099981ebd65ffc89ccd6bd37d842009e13e1
Pauland Ipa <ppawlenchumil@gmail.com>
2026-01-12 18:27:36 +0800

    another attempt to fix the pfp image

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const textElement = document.getElementById('typing-text');
-const words = ["Software Developer", "Student", "Tech Enthusiast"];
+const words = ["Software Developer", "Student", "Web Designer"];
 let wordIndex = 0;
 let charIndex = 0;
 let isDeleting = false;
@@ -19,7 +19,7 @@ function type() {
 
     if (!isDeleting && charIndex === currentWord.length) {
         isDeleting = true;
-        typeSpeed = 2000; // Wait before deleting
+        typeSpeed = 2000;
     } else if (isDeleting && charIndex === 0) {
         isDeleting = false;
         wordIndex = (wordIndex + 1) % words.length;
